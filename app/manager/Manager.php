bdb0fbff348d88b8c2366a2a958872145b409f8d
El houssaïne El jadid <ikky57@gmail.com>
2022-06-10 18:48:53 +0200

    mise a jour manager pdo

--- app/manager/Manager.php
+++ app/manager/Manager.php
@@ -1,22 +1,19 @@
-<?php 
+<?php
 
 abstract class Manager
 {
     protected static $pdo = NULL;
-// Le méthode ci dessous nous permet d'instancier PDO une seul et unique fois et de la partager via l'héritage a nos autres manager
+    // Le méthode ci dessous nous permet d'instancier PDO une seul et unique fois et de la partager via l'héritage a nos autres manager
     protected function getPdo()
     {
-        if(self::$pdo == NULL){ // Si PDO n'est pas instancier
-            try
-            {
+        if (self::$pdo == NULL) { // Si PDO n'est pas instancier
+            try {
                 // Ce connecte a la base de donnée
 
                 // self::$pdo = new PDO('mysql:host=51.77.210.152;dbname=alertmns;port=3306;charset=UTF8','atashi', 'Aedlp1467!');
 
-                self::$pdo = new PDO('mysql:host=localhost;dbname=alertmns', 'root', '');
-            }
-            catch(PDOException $e)
-            {
+                self::$pdo = new PDO('mysql:host=localhost;dbname=alertmns ;charset=utf8', 'root', '');
+            } catch (PDOException $e) {
                 echo 'Error : ' . $e->getMessage();
                 die;
             }
@@ -25,5 +22,4 @@ abstract class Manager
 
         return self::$pdo;
     }
-
 }
